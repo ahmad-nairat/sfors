a39852ea3d5c656c98b9e06e2a2d185fa9646d1f
ahmad-nairat <146556837+ahmad-nairat@users.noreply.github.com>
2024-03-24 06:05:50 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const courses = [
+{
+    courseName: "برمجة 1",
+    sections: [
+        { sectionName: "احد ثلاثاء | 8:30 - 9:30", link: "https://www.google.com" },
+        { sectionName: "احد ثلاثاء | 10:30 - 11:30", link: "https://www.facebook.com" },
+        { sectionName: "اثنين اربعاء | 8:30 - 10:00", link: "https://www.microsoft.com" },
+        { sectionName: "احد ثلاثاء | 10:00 - 11:30", link: "https://www.ahu.edu.jo" },
+    ]
+},
+{
+    courseName: "برمجة 1",
+    sections: [
+        { sectionName: "احد ثلاثاء | 8:30 - 9:30", link: "https://www.google.com" },
+        { sectionName: "احد ثلاثاء | 10:30 - 11:30", link: "https://www.facebook.com" },
+        { sectionName: "اثنين اربعاء | 8:30 - 10:00", link: "https://www.microsoft.com" },
+        { sectionName: "احد ثلاثاء | 10:00 - 11:30", link: "https://www.ahu.edu.jo" },
+    ]
+},
+{
+    courseName: "برمجة شيئية 1",
+    sections: [
+        { sectionName: "احد ثلاثاء | 8:30 - 9:30", link: "https://www.google.com" },
+        { sectionName: "احد ثلاثاء | 10:30 - 11:30", link: "https://www.facebook.com" },
+        { sectionName: "اثنين اربعاء | 8:30 - 10:00", link: "https://www.microsoft.com" },
+        { sectionName: "احد ثلاثاء | 10:00 - 11:30", link: "https://www.ahu.edu.jo" },
+    ]
+},
+{
+    courseName: "مقدمة في هندسة البرمجيات",
+    sections: [
+        { sectionName: "احد ثلاثاء | 8:30 - 9:30", link: "https://www.google.com" },
+        { sectionName: "احد ثلاثاء | 10:30 - 11:30", link: "https://www.facebook.com" },
+        { sectionName: "اثنين اربعاء | 8:30 - 10:00", link: "https://www.microsoft.com" },
+        { sectionName: "احد ثلاثاء | 10:00 - 11:30", link: "https://www.ahu.edu.jo" },
+    ]
+},
+];
+
+function showSections(element){
+    console.log(element.textContent);
+}
